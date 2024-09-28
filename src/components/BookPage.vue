caaf5d967302f26f67f947fbce43d599e2dd0d5b
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-09-28 23:39:14 +0300

    upd

--- src/components/BookPage.vue
+++ src/components/BookPage.vue
@@ -3,12 +3,15 @@
         <div class="chapter">
             <h1>{{ chapter.title }}</h1>
             <div class="content-container">
-                <p>{{ chapter.text }}</p>
+                <div v-if="chapter.text" v-for="(paragraph, index) in chapter.text.split('\n\n')" :key="index">
+                    <p>{{ paragraph }}</p>
+                </div>
                 <div class="tool-container"></div>
             </div>
         </div>
     </div>
 </template>
+
 
 <script>
 import bookJson from '@/assets/book.json';
@@ -16,7 +19,7 @@ import bookJson from '@/assets/book.json';
 export default {
     data() {
         return {
-            chapter: {}
+            chapter: { title: '', text: '' }
         }
     },
     mounted() {
@@ -32,44 +35,44 @@ export default {
 
 <style scoped>
 .chapter-container {
-  display: flex;
-  width: 100%; 
-  margin: 0 auto; 
+    display: flex;
+    width: 100%;
+    margin: 0 auto;
 }
 
 .chapter {
-  flex: 2;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 40px;
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px;
 }
 
 .chapter h1 {
-  margin-bottom: 20px;
+    margin-bottom: 20px;
 }
 
 .content-container {
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 }
 
 .chapter p {
-  background-color: #d4e8d8;
-  padding: 20px;
-  border: 1px solid teal;
-  border-radius: 10px;
-  flex: 6;
-  font-size: 20px;
+    background-color: #d4e8d8;
+    padding: 20px;
+    border: 1px solid teal;
+    border-radius: 10px;
+    font-size: 20px;
 }
 
 .tool-container {
-  width: auto;
-  height: 100%;
-  background-color: #f0f0f0;
-  border: 1px solid #ccc; 
-  border-radius: 10px; 
-  margin-left: 20px;
-  flex: 1;
+    width: auto;
+    height: 80%;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    margin-left: 20px;
+    padding: 50px;
+    margin: 10px;
 }
 </style>
